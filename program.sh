4d0f19968f0b2c9113e2d20bedffc9d291c0a6e5
figwheel-calva-docker <figwheel-calva-docker@program>
2021-07-30 11:31:41 +0300

    i am figwheel-calva-docker program

---
+++ program.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+repl(){
+    clj -A:repl
+}
+
+push(){
+  ORIGIN=$(git remote get-url origin)
+  rm -rf .git
+  git init -b main
+  git remote add origin $ORIGIN
+  git config --local include.path ../.gitconfig
+  git add .
+  git commit -m "i am figwheel-calva-docker program"
+  git push -f -u origin main
+}
+
+main(){
+  clojure -M:main
+}
+
+
+uberjar(){
+
+  lein with-profiles +prod uberjar
+  mkdir -p target/jpackage-input
+  mv target/deathstar.standalone.jar target/jpackage-input/
+  #  java -Dclojure.core.async.pool-size=1 -jar target/deathstar.standalone.jar
+}
+
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting compilation..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut --icon logo/icon.ico"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut --icon logo/logo-728.png"
+  else
+      J_ARG="--icon logo/icon.icns"
+  fi
+
+  APP_VERSION=0.1.0
+
+  jpackage \
+    --input target/jpackage-input \
+    --dest target \
+    --main-jar deathstar.standalone.jar \
+    --name "deathstar" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments deathstar.main \
+    --resource-dir logo \
+    --java-options -Xmx2048m \
+    --app-version ${APP_VERSION} \
+    $J_ARG
+  
+}
+
+"$@"
---
+++ readme.txt
@@ -0,0 +1,3 @@
+i am figwheel-calva-docker program - i repl into figwheel program running inside docker
+
+i have no git history - my source files define me
