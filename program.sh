56004af0cd0a08d3dd38fde613b08cc3a9d673da
figwheel-calva-docker <figwheel-calva-docker@program>
2021-08-01 02:38:07 +0300

    from program.sh i remove no history push

--- program.sh
+++ program.sh
@@ -4,17 +4,6 @@ repl(){
     clj -A:repl
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am figwheel-calva-docker program"
-  git push -f -u origin main
-}
-
 main(){
   clojure -M:main
 }
